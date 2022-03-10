c747e3f0ef46da03a39d3d168892650a1e0d52cc
kindring <2460131004@qq.com>
2022-03-10 08:21:31 +0800

    新版本

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const IndexApi = require('./IndexApi');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+/* 登录页面 */
+router.get('/login',(req,res)=>{
+
+})
+
+router.use('/api',IndexApi);
 
 module.exports = router;
